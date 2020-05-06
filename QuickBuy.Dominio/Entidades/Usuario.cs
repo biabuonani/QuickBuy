ddfcfa179d7e8d3bca422849d95818d0bda8cdb1
beatriz.buonani <beatriz.buonani@DV-17>
2020-05-06 16:42:14 -0300

    Criação das entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Nome { get; set; }
+
+        public string SobreNome { get; set; }
+
+        //Usuario pode ter nenhum ou muitos pedidos
+       public ICollection<Pedido> Pedidos { get; set; }
+
+
+
+
+
+    }
+}
